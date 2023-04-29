b5251750b5d68b17e90d2f69b5c23475f83261cd
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-04-29 20:00:34 -0300

    Update primeiro.js

--- CoderWebDevelopment/Inicio/primeiro.js
+++ CoderWebDevelopment/Inicio/primeiro.js
@@ -2,3 +2,21 @@ console.log("Hello world!");
 console.log("Bom dia");
 //Comentários são similares em geral 
 /* Como visto, console.log é o printf */
+
+//Numeros pares, sem utilizar iteradores
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9); 
+}
+console.log();
+//Numeros Impares, sem utilzar iteradores
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10); 
+}
