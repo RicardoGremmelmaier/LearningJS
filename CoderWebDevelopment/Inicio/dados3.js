d91f2127e48772b01b5991cc753defb54be767f6
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-05-01 00:14:00 -0300

    Update dados3.js

--- CoderWebDevelopment/Inicio/dados3.js
+++ CoderWebDevelopment/Inicio/dados3.js
@@ -2,4 +2,10 @@ let idade = 31;
 let salario = 4578.32;
 
 console.log(typeof idade);
-console.log(typeof salario);
+console.log(typeof salario); //Type number
+
+let isRaining = true; //Ou false, type boolean
+
+console.log(typeof isRaining);
+
+//"" ou '' é tipo string
