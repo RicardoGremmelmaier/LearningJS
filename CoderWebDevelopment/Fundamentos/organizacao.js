789dacd4cbae366a27fb225a6e5ab02452701f3d
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-05-02 20:50:57 -0300

    Update organizacao.js

--- CoderWebDevelopment/Fundamentos/organizacao.js
+++ CoderWebDevelopment/Fundamentos/organizacao.js
@@ -3,3 +3,13 @@ console.log("Sentenças podem ter ou nao ponto e virgula")
 {
     //Bloco serve pra agrupar sentenças de códigos]
 }
+/* variaveis e comparações e atribuições sao similares
+* = atribuição == comparação
+* evite o uso de var, use let
+* const nao altera, constante 
+*/
+/* A tipagem em js é fraca, não quer dizer q nao existe, mas é fraca
+* 9 e 3.8 são number, "legal" 'legal' são strings e variaveis tem um 
+* tipo dinamico mt foda, um string pode ser associado a uma variavel
+* que antes era um number 
+ */
