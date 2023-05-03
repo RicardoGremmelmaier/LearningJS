86b6f6c0b83ee17b75ca02b8cc2cb6b6918a3496
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-05-02 23:27:13 -0300

    Create object.js

---
+++ CoderWebDevelopment/Fundamentos/object.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa polo',
+    preco: 79.90
+}
+
+console.log(prod2)
+
+//JSON uma forma textual de objeto js
+// '{ "nome": "Camisa Polo", "preco": 79.90 }'
