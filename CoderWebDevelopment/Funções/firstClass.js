2993b56ef45c942c8a64a34c6c73c8eba633e913
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-05-14 01:31:37 -0300

    Update firstClass.js

--- CoderWebDevelopment/Funções/firstClass.js
+++ CoderWebDevelopment/Funções/firstClass.js
@@ -31,3 +31,45 @@ function soma(a,b){
 }
 
 soma(2,3)(4)
+
+//Parametros variaveis
+function soma(){
+    let soma = 0
+    for(i in arguments){
+        soma += arguments[i]
+    }
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1.1, 2.2, 3.3))
+console.log(soma(1.1, 2.2, "Teste"))
+
+//Parametro padrao
+//strat 1 valor padrao
+function soma1(a,b,c){
+    a = a || 1
+    b = b || 1
+    c = c || 1
+    return a + b + c
+}
+
+console.log(soma1(1), soma1(3), soma1(1,2,3), soma1(0,0,0))
+
+//strat 2, 3, 4
+function soma2(a,b,c){
+    a = a !== undefined ? a : 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+    return a + b + c
+}
+
+console.log(soma2(), soma2(3), soma2(1,2,3), soma2(0,0,0))
+
+//valor padrao
+function soma3(a = 1, b = 1, c = 1){
+    return a + b + c
+}
+
+console.log(soma3(), soma3(3), soma3(1,2,3), soma3(0,0,0))
