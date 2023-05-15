cc55794766590a08b928e5ba453e0214e00025f0
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-05-14 23:30:49 -0300

    Update ex5.js

--- CoderWebDevelopment/Exercícios 1/ex5.js
+++ CoderWebDevelopment/Exercícios 1/ex5.js
@@ -1,3 +1,6 @@
+// Desenvolva uma função JavaScript para
+//que ela receba um valor como 0.30000000000000004 e retorne R$0,30 (observe a vírgula e o ponto).
+
 function normaliza(valor){
     valorFinal = valor.toFixed(2)
     valorFinal = valorFinal.replace('.', ',')
