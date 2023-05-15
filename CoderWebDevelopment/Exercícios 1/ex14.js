99fc21666e0955f485f66dfbb7dc99dc6303d1bd
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-05-15 19:25:01 -0300

    Update ex14.js

--- CoderWebDevelopment/Exercícios 1/ex14.js
+++ CoderWebDevelopment/Exercícios 1/ex14.js
@@ -2,3 +2,27 @@
 casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com
 escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções .Crie
 também um default, que retornará uma mensagem de erro no console. */
+
+function frutaria(fruta){
+    switch(fruta.toLowerCase()){
+        case('maçã'): 
+            console.log('Não vendemos esta fruta aqui')
+            break
+        case('kiwi'):
+            console.log('Estamos com escassez de kiwis')
+            break
+        case('melancia'):
+            console.log('Aqui está, são 3 reais o quilo')
+            break
+        default:
+            console.log('Error')
+    }
+}
+
+frutaria('maçã')
+frutaria('MAÇÃ')
+frutaria('kiwi')
+frutaria('KIWI')
+frutaria('melancia')
+frutaria('MELANCIA')
+frutaria('banana')
