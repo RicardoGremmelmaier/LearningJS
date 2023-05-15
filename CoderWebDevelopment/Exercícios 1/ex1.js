148ffb96d9ee765632e1eacee81a1bb28be02466
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-05-14 21:25:50 -0300

    Create ex1.js

---
+++ CoderWebDevelopment/Exercícios 1/ex1.js
@@ -0,0 +1,16 @@
+//Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+//multiplicação e divisão desses valores
+
+function matBasica(a,b){
+    const soma = a + b
+    const subtração = a - b
+    const multiplicação = a * b
+    const divisão = a / b
+
+    console.log(soma)
+    console.log(subtração)
+    console.log(multiplicação)
+    console.log(divisão)
+}
+
+matBasica(5,10)
