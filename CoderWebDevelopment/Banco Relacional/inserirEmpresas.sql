5179376e89e6b92f6910ec98affb6f4a82e1f54b
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-05-17 12:34:57 -0300

    Create inserirEmpresas.sql

---
+++ CoderWebDevelopment/Banco Relacional/inserirEmpresas.sql
@@ -0,0 +1,21 @@
+insert into empresas
+    (nome, cnpj)
+values
+    ('Bradesco', 9095213950),
+    ('Vale', 2788714800),
+    ('Cielo', 0145153832);
+
+alter table empresas modify cnpj varchar(20);
+
+desc empresas;
+desc prefeitos;
+select * from empresas;
+select * from cidades;
+
+insert into empresas_unidades
+    (empresa_id, cidade_id, sede)
+values
+    (1, 1, 1),
+    (1, 2, 0),
+    (2, 1, 0),
+    (2, 2, 1);
