bd4f5ccda090dc71c45bc65ce301ab05a5051874
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-07-04 20:09:05 -0300

    Create oo.js

---
+++ CoderWebDevelopment/Objetos/oo.js
@@ -0,0 +1,23 @@
+//CÓDIGO NAO EXECUTAVEL
+
+//PROCEDURAL baseado em procedimentos
+processamento(valor1, valor2, valor3)
+
+// OO 
+objeto ={
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento()
+
+// principios importantes
+
+//1. abstraçao
+//2. encapsulamento
+//3. herança
+//4. polimorfismo
