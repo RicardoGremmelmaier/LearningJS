f36061f8bbcaf7da3f0b87402428a9c2f79562a5
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-07-05 19:05:16 -0300

    Update herança.js

--- CoderWebDevelopment/Objetos/herança.js
+++ CoderWebDevelopment/Objetos/herança.js
@@ -6,10 +6,49 @@
 
  const volvo = {
     modelo: 'V40',
-    velMax: 220,
+    status(){
+      return `${this.modelo}: ${super.status}`
+    }
  }
 
  console.log(ferrari.__proto__)
  console.log(ferrari.__proto__ === Object.prototype)
  console.log(volvo.__proto__ === Object.prototype)
  console.log(Object.prototype.__proto__)
+
+ //Cadeia de prototipos
+ Object.prototype.attr0 = '0'
+ const avo = {attr1: 'A'}
+ const pai = {__proto__: avo, attr2: 'B'}
+ const filho = {__proto__: pai, attr3: 'C'}
+ console.log(filho.attr1)
+ console.log(filho.attr0)
+
+ const carro = {
+   velAtual: 0,
+   velMax: 200,
+   acelerarMais(delta){
+      if(this.velAtual + delta <= this.velMax)
+         this.velAtual += delta
+      else
+         this.velAtual = this.velMax
+   },
+   status(){
+      return `${this.velAtual}Km/h de ${this.velMax}`
+   }
+ }
+
+ //Usando a ferrari,ela da shadowing na velMax do obj carro, similar com status de volvo
+
+ Object.setPrototypeOf(ferrari, carro)
+ Object.setPrototypeOf(volvo, carro)
+
+ console.log(ferrari)
+ console.log(volvo)
+
+ volvo.acelerarMais(100)
+ console.log(volvo.status())
+
+ ferrari.acelerarMais(300)
+ console.log(ferrari.status()
+ )
