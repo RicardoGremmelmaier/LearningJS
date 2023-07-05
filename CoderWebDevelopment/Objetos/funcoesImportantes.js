25f41fa6de5cb4f7acff9c03a950ac19c0963845
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-07-04 21:44:53 -0300

    Create funcoesImportantes.js

---
+++ CoderWebDevelopment/Objetos/funcoesImportantes.js
@@ -0,0 +1,28 @@
+const pessoa = {
+    nome: 'Rebeca',
+    idade: 2,
+    peso: 13
+}
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
+
+Object.entries(pessoa).forEach(([chave, valor]) => {
+    console.log(chave + ': ' + valor)
+})
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true,
+    writable: false,
+    value: '01/01/2023'
+})
+
+pessoa.dataNascimento = '01/01/2020'
+console.log(pessoa.dataNascimento) //2023 
+console.log(pessoa.keys(pessoa))
+
+const dest = {a: 1}
+const o1 = {b: 2}
+const o2 = {c: 3, a: 4}
+const obj = Object.assign(dest, o1, o2)  
