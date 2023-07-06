603ec9d96dafec1ae6a62d10b3e1f16aa1dcb1d3
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-07-06 17:01:12 -0300

    Update JSONvsObj.js

--- CoderWebDevelopment/Objetos/JSONvsObj.js
+++ CoderWebDevelopment/Objetos/JSONvsObj.js
@@ -1,1 +1,6 @@
 //JSON é formato de dados, e não um objeto, é um formato textual, simples e usado parar ler em qualquer linguagem
+
+const obj = {a: 1, b: 2, c: 3, soma(){return a+b+c}}
+console.log(JSON.stringify(obj))
+
+console.log(JSON.parse('{"a":1 , "b": 2, "c": 3}'))//Da pra colocar array, obj, string dentro de aspas, etc
