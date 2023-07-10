5450f9347feba5a9e6c7eccd22691dd7a547b828
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-07-10 19:04:12 -0300

    Create ex6.js

---
+++ CoderWebDevelopment/Exercícios Fundamentos 2/ex6.js
@@ -0,0 +1,9 @@
+//func q recebe um numero como param e retorna uma string de +, com a quantidade do num recebido
+
+function simboloMais(quantidade){
+    return '+'.repeat(quantidade)
+}
+
+console.log(simboloMais(2))
+console.log(simboloMais(4))
+console.log(simboloMais(0))
