c6533f35457d4229c9711f8c4846aba9d68e2f5b
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-07-10 14:31:12 -0300

    Update reduce.js

--- CoderWebDevelopment/Array/reduce.js
+++ CoderWebDevelopment/Array/reduce.js
@@ -9,3 +9,30 @@ const resultado = (alunos.map(a => a.nota).reduce(function(acumulador, atual){
     console.log(acumulador + atual)
     return acumulador + atual
 })) //adicionei um valor inicial
+
+
+//desafio: tdos os alunos sao bolsistas?
+const todosBolsistas = function (resultado, bolsista){
+        return resultado && bolsista
+} 
+
+const desafio1 = (alunos.map(a => a.bolsista).reduce(todosBolsistas))
+console.log(desafio1)
+
+//desafio: algum aluno é bolsista?
+const temBolsistas = function (resultado, bolsista){
+    return resultado || bolsista
+} 
+
+const desafio2 = (alunos.map(a => a.bolsista).reduce(temBolsistas))
+console.log(desafio2)
+
+//Implementando
+
+Array.prototype.reduce2 = function(callback){
+    let acumulador = 0
+    for(let i = 1; i < this.length; i++){
+        acumulador = callback(acumulador, this[i], i, this)
+    }
+    return acumulador
+}
