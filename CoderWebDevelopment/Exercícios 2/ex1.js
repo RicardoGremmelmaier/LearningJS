10969510256901698f87fe72232408030690d306
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-07-10 17:20:03 -0300

    Create ex1.js

---
+++ CoderWebDevelopment/Exercícios 2/ex1.js
@@ -0,0 +1,7 @@
+//Função que retorna a string "Olá, " concatenada com um argumento text recebido como parametro e com ponto de exclamação "!" no final
+
+function cumprimentar(argumento) {
+    return "Olá, " + argumento + "!"
+}
+
+console.log(cumprimentar('Ricardo'))
