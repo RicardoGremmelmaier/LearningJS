9a34c35340506b3288e2b572c93f4ef63db89229
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2023-07-11 18:08:29 -0300

    Update callApply.js

--- CoderWebDevelopment/Funções/callApply.js
+++ CoderWebDevelopment/Funções/callApply.js
@@ -16,5 +16,5 @@ const carro = {preco: 49990, desc: 0.20}
 console.log(getPreco.call(carro))
 console.log(getPreco.apply(carro))
 
-console.log(getPreco.apply(carro, 0.17, '$'))
+console.log(getPreco.call(carro, 0.17, '$'))
 console.log(getPreco.apply(carro, [0.15, '$']))
